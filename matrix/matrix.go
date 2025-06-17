20797f6e4b9ec3816ef63d5b5ff0e8cfb26f4b77
Jonathan Landry <jonathan.landry@etu.u-bordeaux.fr>
2025-06-17 17:31:54 +0200

    Adding basic definition for Vector and Matrix types

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,1 +1,72 @@
 package matrix
+
+type Matrix struct {
+	data   [][]float64
+	nbRows int
+	nbCols int
+}
+
+func (m *Matrix) GetData() [][]float64 {
+	return m.data
+}
+
+func (m *Matrix) GetNbRows() int {
+	return m.nbRows
+}
+
+func (m *Matrix) GetNbCols() int {
+	return m.nbCols
+}
+
+func (m *Matrix) IsSquareMatrix() bool {
+	return m.nbCols == m.nbRows
+}
+
+func (m *Matrix) GetElementAt(row int, col int) float64 {
+	return m.data[row][col]
+}
+
+func (m *Matrix) SetElementAt(row int, col int, elt float64) {
+	m.data[row][col] = elt
+}
+
+func New(nbRowsMat int, nbColsMat int) *Matrix {
+	data := make([][]float64, nbRowsMat)
+	for i := range data {
+		data[i] = make([]float64, nbColsMat)
+	}
+	return &Matrix{
+		data:   data,
+		nbRows: nbRowsMat,
+		nbCols: nbColsMat,
+	}
+}
+
+func NewFromData(mData [][]float64) *Matrix {
+	nbRows := len(mData)
+	if nbRows == 0 {
+		return &Matrix{
+			data:   [][]float64{},
+			nbRows: 0,
+			nbCols: 0,
+		}
+	}
+
+	nbCols := len(mData[0])
+
+	// Make a deep copy
+	copiedMatrix := make([][]float64, nbRows)
+	for i := range mData {
+		if len(mData[i]) != nbCols {
+			panic("inconsistent number of columns in input data")
+		}
+		copiedMatrix[i] = make([]float64, nbCols)
+		copy(copiedMatrix[i], mData[i])
+	}
+
+	return &Matrix{
+		data:   copiedMatrix,
+		nbRows: nbRows,
+		nbCols: nbCols,
+	}
+}
--- vector/vector.go
+++ vector/vector.go
@@ -1,1 +1,36 @@
 package vector
+
+type Vector struct {
+	data []float64
+	size int
+}
+
+func (v *Vector) GetData() []float64 {
+	return v.data
+}
+
+func (v *Vector) GetSize() int {
+	return v.size
+}
+
+func (v *Vector) GetElementAt(idx int) float64 {
+	return v.data[idx]
+}
+
+func (v *Vector) SetElementAt(idx int, elt float64) {
+	v.data[idx] = elt
+}
+
+func New(vSize int) *Vector {
+	return &Vector{data: make([]float64, vSize), size: vSize}
+}
+
+func NewFromData(vData []float64) *Vector {
+	copiedVector := make([]float64, len(vData))
+	copy(copiedVector, vData)
+
+	return &Vector{
+		data: copiedVector,
+		size: len(copiedVector),
+	}
+}
